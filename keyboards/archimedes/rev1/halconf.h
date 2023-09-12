131ec55a57fc01769ad16791cce749459dde8851
SpandexWizard <spandexwizard@proton.me>
2023-09-11 22:50:31 -0500

    bug fixes

--- keyboards/archimedes/rev1/halconf.h
+++ keyboards/archimedes/rev1/halconf.h
@@ -1,6 +1,8 @@
 // Copyright 2023 SpandexWizard (@SpandexWizard)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include_next <halconf.h>
+
 #define HAL_USE_SPI TRUE
 #define SPI_USE_WAIT TRUE
 #define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
